5ee8957245949215f097dce624385de1c7318319
nwjgit <nwjgit@gmail.com>
2023-11-27 21:59:03 -0600

    Fancier boots

--- src/mahoji/lib/abstracted_commands/strongHoldOfSecurityCommand.ts
+++ src/mahoji/lib/abstracted_commands/strongHoldOfSecurityCommand.ts
@@ -1,4 +1,5 @@
 import { Time } from 'e';
+import { Bank } from 'oldschooljs';
 
 import { prisma } from '../../../lib/settings/prisma';
 import type { ActivityTaskOptionsWithNoChanges } from '../../../lib/types/minions';
@@ -15,8 +16,25 @@ export async function strongHoldOfSecurityCommand(user: MUser, channelID: string
 			type: 'StrongholdOfSecurity'
 		}
 	});
+	const missingBoots = new Bank();
 	if (count !== 0) {
-		return "You've already completed the Stronghold of Security!";
+		if (!user.bank.has('Fancy boots')) {
+			missingBoots.add('Fancy boots');
+		}
+		if (!user.bank.has('Fighting boots')) {
+			missingBoots.add('Fighting boots');
+		}
+		if (!user.bank.has('Fancier boots')) {
+			missingBoots.add('Fancier boots');
+		}
+		if (missingBoots.items.length > 0) {
+			await transactItems({ userID: user.id, itemsToAdd: missingBoots });
+			const addedBoots = missingBoots.items().map(item => item[0].name);
+
+			return addedBoots.length > 0
+				? `You've already completed the Stronghold of Security! Added missing boots: ${addedBoots.join(', ')}`
+				: "You've already completed the Stronghold of Security!";
+		}
 	}
 
 	await addSubTaskToActivityTask<ActivityTaskOptionsWithNoChanges>({
