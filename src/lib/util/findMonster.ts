a082a50e8befa4073127374cf1835fc6cfa70994
nwjgit <nwjgit@gmail.com>
2024-01-14 23:34:43 -0600

    NexMonster, KalphiteKingMonster, Naxxus show_info

--- src/lib/util/findMonster.ts
+++ src/lib/util/findMonster.ts
@@ -1,13 +1,21 @@
 import killableMonsters from '../minions/data/killableMonsters';
+import { KalphiteKingMonster } from '../minions/data/killableMonsters/custom/bosses/KalphiteKing';
+import { Naxxus } from '../minions/data/killableMonsters/custom/bosses/Naxxus';
 import { KillableMonster } from '../minions/types';
+import { NexMonster } from '../nex';
 import { stringMatches } from '../util';
 
 export default function findMonster(str = ''): KillableMonster | undefined {
-	const mon = killableMonsters.find(
+	// Ignecarus, KingGoldemar, VasaMagus aren't setup as KillableMonster
+	const allMonsters: KillableMonster[] = [NexMonster, KalphiteKingMonster, Naxxus, ...killableMonsters];
+
+	const mon = allMonsters.find(
 		mon =>
 			stringMatches(mon.id.toString(), str) ||
 			stringMatches(mon.name, str) ||
-			mon.aliases.some(alias => stringMatches(alias, str))
+			(mon.aliases && mon.aliases.some(alias => stringMatches(alias, str)))
 	);
+
+	console.log(mon);
 	return mon;
 }
